337c01d3029d6dd98f211eca7e1c655114207b3b
samuelboerner <boernersam@gmail.com>
2024-04-20 11:26:24 -0700

    initial commit

---
+++ tests/test_JangParser.py
@@ -0,0 +1,23 @@
+import sys
+import unittest
+
+sys.path.append('../src')
+import JangLexer as JL
+from JangParser import JangParser
+
+class TestJangParser(unittest.TestCase):
+    def setUp(self):
+        pass
+
+    def test_variable_declaration(self):
+        pass
+
+    def test_function_declaration(self):
+        pass
+
+    def test_variable_change(self):
+        pass
+
+# To run the tests
+if __name__ == '__main__':
+    unittest.main()

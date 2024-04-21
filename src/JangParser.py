4a987380dfd41449c669ff069adc548b719483d2
samuelboerner <boernersam@gmail.com>
2024-04-20 17:44:28 -0700

    basic function declaration

--- src/JangParser.py
+++ src/JangParser.py
@@ -27,40 +27,54 @@ class JangParser:
         self.cursor = Cursor
 
         self.variables = {}
+        self.functions = {}
 
     def Parse(self, tokens):
         self.cursor = Cursor(tokens)
+
         while self.cursor.isTokensLeft():
+
+            # First token consumed
             token = self.cursor.getToken()
+            # Begin parsing with second token in token list
+
             if token.type == 'NEWLINE' or token.type == 'EOF':
                 self.cursor.advanceToken()
+
             if token.type == 'FUNC_DCL':
-                # self.parse_function_declaration()
-                pass
+                self.parseFunctionDeclaration()
+
             elif token.type == 'IMPORT':
                 # self.parse_import_statement()
                 pass
+
             elif token.type == 'VAR_DCL':
                 self.parseVariableDeclaration()
+
             elif token.type == 'VAR_CHANGE':
                 # self.parse_variable_change()
                 pass
+
             elif token.type == 'RETURN':
-                # self.parse_return_statement()
                 pass
+
             elif token.type == 'PRINT':
                 # self.parse_print_statement()
                 pass
+
             elif token.type == 'CLASS':
                 # self.parse_class_declaration()
                 pass
+
             elif token.type == 'IDENTIFIER':
                 # if the first token is an identifier, its most likely a function call
                 # self.parse_function_call()
                 pass
+
             elif token.type == 'IF':
                 # self.parse_conditional()
                 pass
+            
             else:
                 self.cursor.advanceToken()  # Move past unrecognized tokens
 
@@ -77,3 +91,28 @@ class JangParser:
         
         value = self.cursor.advanceToken().value
         self.variables[name] = (type_, value)
+
+    def parseFunctionDeclaration(self):
+        returnType = self.cursor.advanceToken().value
+        name = self.cursor.advanceToken().value
+
+        if self.cursor.advanceToken().type != 'LPAREN':
+            raise RuntimeError('Expected LPAREN after function name')
+        if self.cursor.advanceToken().type == 'RPAREN':
+            args = []
+        else:
+            args = self.parseArguments()
+        body = self.parseBody()
+
+        self.functions[name] = {
+            'returnType': returnType,
+            'args': args,
+            'body': body 
+        }
+
+    def parseArguments(self):
+        pass
+
+    def parseBody(self):
+        pass
+        
